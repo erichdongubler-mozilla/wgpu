d61dba431b9791d799b097570df32f357bd2ce79
Erich Gubler <erichdongubler@gmail.com>
2024-10-10 14:28:42 -0400

    WIP: refactor(core): use `std` instead of `once_cell`
    Currently only possible on Nightly Rust, because `wgpu-core` needs
    [`std::sync::OnceLock::get_or_try_init`].
    
    [`std::sync::OnceLock::get_or_try_init`]: https://doc.rust-lang.org/stable/std/sync/struct.OnceLock.html#method.get_or_try_init

--- wgpu-core/src/pool.rs
+++ wgpu-core/src/pool.rs
@@ -1,14 +1,13 @@
-use alloc::sync::{Arc, Weak};
+use alloc::sync::{Arc, OnceLock, Weak};
 use core::hash::Hash;
 
 use hashbrown::{hash_map::Entry, HashMap};
-use once_cell::sync::OnceCell;
 
 use crate::lock::{rank, Mutex};
 use crate::FastHashMap;
 
 type SlotInner<V> = Weak<V>;
-type ResourcePoolSlot<V> = Arc<OnceCell<SlotInner<V>>>;
+type ResourcePoolSlot<V> = Arc<OnceLock<SlotInner<V>>>;
 
 pub struct ResourcePool<K, V> {
     inner: Mutex<FastHashMap<K, ResourcePoolSlot<V>>>,
@@ -50,7 +49,7 @@ impl<K: Clone + Eq + Hash, V> ResourcePool<K, V> {
                 // No entry exists for this resource.
                 //
                 // We know that the resource is not alive, so we can create a new entry.
-                Entry::Vacant(entry) => Arc::clone(entry.insert(Arc::new(OnceCell::new()))),
+                Entry::Vacant(entry) => Arc::clone(entry.insert(Arc::new(OnceLock::new()))),
             };
 
             drop(map_guard);
