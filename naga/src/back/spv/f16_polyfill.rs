d29f159d5aa85c3094b7d55efbe8d9aae57c645f
Erich Gubler <erichdongubler@gmail.com>
2025-07-16 17:08:57 -0400

    docs(vulkan-out): add internal docs for `f32_local_ty`

--- naga/src/back/spv/f16_polyfill.rs
+++ naga/src/back/spv/f16_polyfill.rs
@@ -22,6 +22,8 @@ fn is_f16(scalar: &crate::Scalar) -> bool {
     scalar.kind == crate::ScalarKind::Float && scalar.width == 2
 }
 
+/// Returns a `LocalType` converting the given `ty_inner` scalar elements from `f16` to `f32`. If
+/// the `ty_inner` is not an `f16` scalar or vector of `f16` scalars, returns `None`.
 pub(in crate::back::spv) fn f32_local_ty(ty_inner: &crate::TypeInner) -> Option<LocalType> {
     use crate::TypeInner;
 
