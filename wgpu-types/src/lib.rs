1bfddec4071f0dcb9e190da6edb44253f500a611
Erich Gubler <erichdongubler@gmail.com>
2025-10-12 23:44:26 -0400

    DO NOT MERGE: prepare for `default_field_values` from Nightly

--- wgpu-types/src/lib.rs
+++ wgpu-types/src/lib.rs
@@ -1,6 +1,7 @@
 //! This library describes the API surface of WebGPU that is agnostic of the backend.
 //! This API is used for targeting both Web and Native.
 
+#![feature(default_field_values)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![allow(
     // We don't use syntax sugar where it's not necessary.
