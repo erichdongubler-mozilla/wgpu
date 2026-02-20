f7834502b02f5e5f7b74ce5e761fc33316783df3
Erich Gubler <erichdongubler@gmail.com>
2026-02-19 20:58:26 -0500

    blerg

--- wgpu-core/src/buffer_region_overrun.rs
+++ wgpu-core/src/buffer_region_overrun.rs
@@ -1,14 +1,12 @@
+use core::{
+    fmt::{self, Display, Formatter},
+    ops::Range,
+};
+
 use thiserror::Error;
 use wgt::BufferAddress;
 
 use crate::command::{CopySide, TransferError};
-
-#[derive(Clone, Debug, Error)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub enum BufferRegion {
-    Range(BufferRange),
-    Whole { buffer_size: BufferAddress },
-}
 
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -19,66 +17,28 @@ pub struct BufferRange {
 
 #[derive(Clone, Debug, Error)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct BufferRegionOutOfBoundsError {
-    pub range: BufferRange,
-    pub region: BufferRegion,
-    pub kind: BufferRegionOutOfBoundsErrorKind,
+pub enum BufferAvailability {
+    ActiveRange(Range<BufferAddress>),
+    Whole { buffer_size: BufferAddress },
 }
 
-#[derive(Clone, Debug)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[non_exhaustive]
-pub enum BufferRegionOutOfBoundsErrorKind {
-    StartOffsetUnderrun,
-    StartOffsetOverrun,
-    EndOffsetOverrun,
-}
-
-/// Error encountered while checking offsets against a buffer.
-#[derive(Clone, Debug, Error)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[non_exhaustive]
-pub enum BufferRegionOverrunError {
-    #[error("start offset ({offset}) would underrun of size {buffer_size}")]
-    StartOffsetUnderrun {
-        start_offset: BufferAddress,
-        region: BufferRegion,
-    },
-    #[error("start offset ({offset}) is out-of-bounds for buffer of size {buffer_size}")]
-    StartOffsetOverrun {
-        offset: BufferAddress,
-        buffer_size: BufferAddress,
-    },
-    #[error(
-        "end offset (start at {} + size of {}) is out-of-bounds for buffer of size {}",
-        offset,
-        size,
-        buffer_size
-    )]
-    EndOffsetOverrun {
-        offset: BufferAddress,
-        size: BufferAddress,
-        buffer_size: BufferAddress,
-    },
-}
-
-impl BufferRegionOverrunError {
+impl BufferAvailability {
     /// Returns the end offset if everything is validated.
     ///
     /// TODO: more
     pub(crate) fn check(
+        &self,
         start_offset: BufferAddress,
         size: BufferAddress,
-        buffer_size: BufferAddress,
-    ) -> Result<BufferAddress, Self> {
-        Self::check_start(start_offset, buffer_size)?.check_end(size)
+    ) -> Result<BufferAddress, BufferRangeOutOfBoundsError> {
+        self.check_start(start_offset)?.check_end(size)
     }
 
     pub(crate) fn check_start(
+        &self,
         start_offset: BufferAddress,
-        buffer_size: BufferAddress,
-    ) -> Result<BufferOverrunEndOffsetChecker, Self> {
-        if start_offset >= buffer_size {
+    ) -> Result<BufferOverrunEndOffsetChecker, BufferRangeOutOfBoundsError> {
+        if start_offset > buffer_size {
             return Err(Self::StartOffsetOverrun {
                 offset: start_offset,
                 buffer_size,
@@ -89,6 +49,54 @@ impl BufferRegionOverrunError {
             buffer_size,
         })
     }
+}
+
+/// Error encountered while checking offsets against a buffer.
+#[derive(Clone, Debug, Error)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct BufferRangeOutOfBoundsError {
+    pub range: BufferRange,
+    pub availability: BufferAvailability,
+    pub kind: BufferRangeOutOfBoundsErrorKind,
+}
+
+impl Display for BufferRangeOutOfBoundsError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let Self {
+            range: BufferRange { start_offset, size },
+            availability,
+            kind,
+        } = self;
+        let offset_at_start: &dyn Fn(&mut Formatter<'_>) -> fmt::Result =
+            &|f| write!(f, "start offset ({start_offset})");
+        let offset_at_end: &dyn Fn(&mut Formatter<'_>) -> fmt::Result =
+            &|f| write!(f, "start offset ({start_offset}) + size ({size})");
+        let (write_offset, direction_desc) = match kind {
+            BufferRangeOutOfBoundsErrorKind::StartUnderrun => (offset_at_start, "under"),
+            BufferRangeOutOfBoundsErrorKind::StartOverrun => (offset_at_start, "over"),
+            BufferRangeOutOfBoundsErrorKind::EndOverrun => (offset_at_end, "over"),
+        };
+        (write_offset)(f)?;
+        write!(f, " would {}run ", direction_desc)?;
+        match availability {
+            BufferAvailability::ActiveRange(buffer_range) => {
+                write!(f, "active buffer range {buffer_range:?}")
+            }
+            BufferAvailability::Whole { buffer_size } => {
+                write!(f, "buffer of size {buffer_size}")
+            }
+        }?;
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[non_exhaustive]
+pub enum BufferRangeOutOfBoundsErrorKind {
+    StartUnderrun,
+    StartOverrun,
+    EndOverrun,
 }
 
 /// A
